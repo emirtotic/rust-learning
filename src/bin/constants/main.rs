193052dd1fb2e9db2bcd7914f43d0359d82d48f6
emirtotic <emirtotic@gmail.com>
2024-12-31 13:17:54 +0100

    mutability and constants

---
+++ src/bin/constants/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let _x: i32 = 5;
+
+    println!("Value of _x is {}", _x);
+    println!("Value of PI is {} and it is declared out of main method. :)", PI);
+    println!("One hour has {} seconds. :)", HOUR_IN_SECONDS);
+
+}
+
+const PI: f64 = 3.14;
+const HOUR_IN_SECONDS: i32 = 60 * 60;
---
+++ src/bin/mutability/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+
+    let mut _a = 5;
+    println!("Value is: {}", _a);
+
+    _a = 10;
+    println!("Value is: {}", _a);
+
+}
